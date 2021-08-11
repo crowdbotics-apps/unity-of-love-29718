26965e3fabda592f1aff1399b62607dd834e2d20
Crowdbotics <team@crowdbotics.com>
2021-08-11 10:42:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "unity_of_love_29718Identifier",
+  url: "https://unity_of_love_29718Identifier.botics.co",
+  api: "https://unity_of_love_29718Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
